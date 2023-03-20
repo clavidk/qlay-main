3a67307d363ce3e30a1b3ac4b1df84a481ed5de9
David Kim <clavid87@gmail.com>
2023-03-20 14:17:04 -0700

    working script with test constraints in place (only 1 page, only 1 article)

--- scripts/scrape.ts
+++ scripts/scrape.ts
@@ -1,11 +1,14 @@
+import { PiperArticle } from '@/types';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
+import {encode} from 'gpt-3-encoder';
 
-const BASE_URL = "https://www.desiringgod.org/authors/john-piper";
+const BASE_URL = "https://www.desiringgod.org";
+const INIT_URL_PATH = "/authors/john-piper";
 const LINK_SELECTOR = 'a.card__shadow';
 const TITLE_SELECTOR = 'h2.card--resource__title';
 const DATE_SELECTOR = 'div.card--resource__date';
-const NEXT_SELECTOR = 'a[rel=next]';
+const NEXT_SELECTOR = 'a[rel="next"]';
 
 const formatDate = (dateString) => {
     const date = new Date(dateString);
@@ -15,13 +18,13 @@ const formatDate = (dateString) => {
     return `${year}-${month}-${day}`;
 };
 
-const scrapeLinks = async (url) => {
+const scrapeLinks = async (url, pageCounter) => {
     const html = await axios.get(url);
     const $ = cheerio.load(html.data);
 
-    const cards = $(`.card--resource`)
+    const cards = $(`.card--resource`);
     const linkArr = [];
-    
+
     cards.each((i, card) => {
         const links = $(card).find(LINK_SELECTOR);
         const title = $(card).find(TITLE_SELECTOR).text().trim();
@@ -37,30 +40,56 @@ const scrapeLinks = async (url) => {
             linkArr.push(linkObj);
         });
     });
-    
+
+    const nextLink = $(NEXT_SELECTOR).attr('href');
+    if (nextLink && pageCounter < 1) {
+        const nextLinks = await scrapeLinks(BASE_URL + nextLink, pageCounter + 1);
+        linkArr.push(...nextLinks);
+    };
+
     return linkArr;
 };
 
-const getAllLinks = async () => {
-    const allLinks = [];
-    let currentPage = BASE_URL;
-    
-    while (true) {
-        console.log(`Scraping links from page: ${currentPage}`);
-        
-        const linkArr = await scrapeLinks(currentPage);
-        allLinks.push(...linkArr);
-        
-        const nextLink = $(NEXT_SELECTOR).attr('href');
-        if (!nextLink) break;
-        
-        currentPage = `https://www.desiringgod.org${nextLink}`;
-    }
-    
-    console.log('All links have been scraped:');
-    console.log(allLinks);
-}
+const getArticle = async (url: string, title: string, date: string) => {
+    let article: PiperArticle = {
+        title: "",
+        url: "",
+        date: "",
+        content: "",
+        tokens: 0,
+        chunks: []
+    };
+
+    const html = await axios.get(`${BASE_URL}/${url}`);
+    const $ = cheerio.load(html.data);
+
+    const body = $(`.resource__body`);
+    const text = $(body).text();
+
+    let cleanedText = text.replace(/\s+/g, " ");
+    cleanedText = cleanedText.replace(/\.([a-zA-Z])/g, ". $1");
+
+    article = {
+        title: title,
+        url: `${BASE_URL}/${url}`,
+        date: date,
+        content: cleanedText,
+        tokens: encode(text).length,
+        chunks: []
+    };
+
+    return article;
+};
+
 
 (async () => {
-    await getAllLinks();
+    const links = await scrapeLinks(BASE_URL + INIT_URL_PATH, 0);
+
+    for(let i = 0; i < links.length; i++) {
+        const link = links[i]
+
+        if (i !== 0) break;
+        const article = await getArticle(link.url, link.title, link.date);
+        console.log(article);
+    }
 })();
