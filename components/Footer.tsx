0e35fe2ee7d114696f95ac0495601bb2ca1ee5a4
David Kim <clavid87@gmail.com>
2023-04-24 22:19:29 -0700

    fix mobile footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,7 +25,7 @@ export const Footer: FC<FooterProps> = ({ show }) => {
     <Transition in={show} timeout={duration} unmountOnExit>
       {(state) => (
         <div
-          className="flex flex-col min-h-[50px] border-t border-gray-300 py-2 px-8 items-center sm:justify-between justify-center"
+          className="flex flex-col min-h-[50px] border-t border-gray-300 py-2 px-4 sm:px-8 items-center sm:justify-between justify-center"
           style={{
             ...defaultStyle,
             ...transitionStyles[state],
@@ -33,9 +33,9 @@ export const Footer: FC<FooterProps> = ({ show }) => {
         >
           <div className="hidden sm:flex"></div>
           <h1 className="text-2xl font-bold mt-2">Follow us</h1>
-          <p className="text-sm text-gray-500 mb-4">We&apos;re building AI-powered tools for everday Christians</p>
+          <p className="text-sm text-gray-500 mb-4">We&apos;re building AI-powered tools for everyday Christians</p>
           <Form />
-          <div className="flex space-x-4 mb-4"></div>
+          <div className="flex space-x-4 mb-4 sm:mb-0"></div>
         </div>
       )}
     </Transition>
