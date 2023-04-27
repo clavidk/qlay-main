e2563149d7cb86d412114a9292b003ed80d5b4dc
David Kim <clavid87@gmail.com>
2023-04-27 09:12:14 -0700

    fix gtm bug

--- components/GoogleTagManager.tsx
+++ components/GoogleTagManager.tsx
@@ -7,15 +7,23 @@ const GTM_ID = 'GTM-5XNMQVM';
 
 const GoogleTagManagerHead = () => {
     return (
+        <>
         <Script
-        id="gtm-script"
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtm.js?id=${GTM_ID}`}
-        onLoad={() => {
-            (window as any).dataLayer = (window as any).dataLayer || [];
-            (window as any).dataLayer.push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-        }}
+            id="gtm-dataLayer"
+            strategy="lazyOnload"
+            dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+            })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+            }}
         />
+        <Script
+            id="gtm-script"
+            strategy="lazyOnload"
+            src={`https://www.googletagmanager.com/gtm.js?id=${GTM_ID}`}
+        />
+        </>
     );
     };
 
