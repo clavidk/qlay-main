851920923d5aad83e8156fdef58ff8c05f0543c8
David Kim <clavid87@gmail.com>
2023-04-26 23:19:57 -0700

    undo changes of recent commits

--- next.config.js
+++ next.config.js
@@ -13,26 +13,6 @@ module.exports = {
         source: '/pipergpt',
         destination: '/pipergpt',
       },
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'pipergpt.clayte.xyz',
-          },
-        ],
-        destination: '/pipergpt/:path*',
-      },
-      {
-        source: '/',
-        has: [
-          {
-            type: 'host',
-            value: 'pipergpt.clayte.xyz',
-          },
-        ],
-        destination: '/pipergpt',
-      },
     ];
   },
 };
