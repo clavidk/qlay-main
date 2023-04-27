74d4313c5c2f7f1c3a9dc9c53b577d49f64867d7
David Kim <clavid87@gmail.com>
2023-04-27 09:36:26 -0700

    add loader to email submit button

--- components/Form.tsx
+++ components/Form.tsx
@@ -6,6 +6,7 @@ export const Form: FC = () => {
     const [email, setEmail] = useState("");
     const [error, setError] = useState("");
     const [success, setSuccess] = useState("");
+    const [isSubmitting, setIsSubmitting] = useState(false);
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -17,53 +18,77 @@ export const Form: FC = () => {
         setError("Please enter a valid email address.");
         return;
         }
-    
+
         setError("");
-        // Handle form submission here
+        setIsSubmitting(true);
 
         try {
-            const response = await fetch("/api/submitForm", {
+        const response = await fetch("/api/submitForm", {
             method: "POST",
             headers: {
-                "Content-Type": "application/json",
+            "Content-Type": "application/json",
             },
             body: JSON.stringify({ name, email }),
-            });
-    
-            if (response.ok) {
+        });
+
+        if (response.ok) {
             setSuccess("Success!");
-            } else {
-            setError("Failed to submit the form. Please try again.");
-            }
-        } catch (error) {
+        } else {
             setError("Failed to submit the form. Please try again.");
         }
+        } catch (error) {
+        setError("Failed to submit the form. Please try again.");
+        } finally {
+        setIsSubmitting(false);
+        }
     };
-    
 
     return (
         <form onSubmit={handleSubmit} className="flex flex-col space-y-2 items-center">
-            <div className="flex space-x-0 items-center">
-                <input
-                    id="email"
-                    type="email"
-                    autoComplete="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    className="block w-full lg:w-auto p-2 border border-gray-300 text-sm"
-                    placeholder="Email"
-                />
-                <button
-                    type="submit"
-                    className="py-2 px-8 text-white text-sm bg-red-600 border border-red-600 hover:bg-red-700"
+        <div className="flex space-x-0 items-center">
+            <input
+            id="email"
+            type="email"
+            autoComplete="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="block w-full lg:w-auto p-2 border border-gray-300 text-sm"
+            placeholder="Email"
+            />
+            <button
+            type="submit"
+            className="py-2 px-8 text-white text-sm bg-red-600 border border-red-600 hover:bg-red-700 flex justify-center items-center"
+            disabled={isSubmitting}
+            style={{ minWidth: '96px', minHeight: '32px' }}
+            >
+            {isSubmitting ? (
+                <svg
+                className="animate-spin h-5 w-5 text-white"
+                viewBox="0 0 24 24"
                 >
-                    Submit
-                </button>
-            </div>
-            {error && <p className="text-red-600 text-sm">{error}</p>}
-            {success && <p className="text-green-600 text-sm">{success}</p>}
+                <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                ></circle>
+                <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l1-1.647z"
+                ></path>
+                </svg>
+            ) : (
+                'Submit'
+            )}
+            </button>
+        </div>
+        {error && <p className="text-red-600 text-sm">{error}</p>}
+        {success && <p className="text-green-600 text-sm">{success}</p>}
         </form>
     );
 };
-    
+
 export default Form;
