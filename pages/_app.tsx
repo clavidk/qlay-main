5b9e0b415bc292d2eb1e62420bc16f13a35b3174
David Kim <clavid87@gmail.com>
2023-04-29 15:18:32 -0700

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import { Inter } from "@next/font/google";
 import type { AppProps } from 'next/app'
 import { GoogleTagManagerHead } from '../components/GoogleTagManager';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps<{}>) {
     <main className={inter.className}>
       <GoogleTagManagerHead />
       <Component {...pageProps} />
+      <Analytics />
     </main>
   );
 }
