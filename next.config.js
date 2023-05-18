6815bc1771e066b38ff9dd44ef75318ff317877e
David Kim <clavid87@gmail.com>
2023-05-18 16:55:52 -0700

    cleanup

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+}
+
+module.exports = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  rewrites: async () => [
-    {
-      source: "/qlay.xyz",
-      destination: "/",
-    },
-    {
-      source: "/pipergpt.qlay.xyz",
-      destination: "/piper",
-    },
-  ]
-}
-
-module.exports = nextConfig;
+  async redirects() {
+    return [
+      {
+        source: '/pipergpt',
+        destination: 'https://pipergpt.qlay.xyz',
+        permanent: true,
+      },
+    ]
+  },
+};
