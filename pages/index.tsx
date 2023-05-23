8b02f0953e76881cab14d94f5e01d8a2c3d7731a
David Kim <clavid87@gmail.com>
2023-05-22 21:31:07 -0700

    break up into new lines

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,14 +12,14 @@ const Home: FC = () => (
       <meta name="description" content="Tools for the sojourner life" />
     </Head>
     <Container>
-      <div>
+      <div className="my-8 text-center">
         <h1 className="text-5xl font-light font-serif mb-4 items-center">QLAY</h1>
       </div>
-      <div>
+      <div className="my-8 text-center">
         <Form />
       </div>
-      <div>
-        <a href="https://pipergpt.qlay.xyz">JohnPiperGPT</a>
+      <div className="my-8 text-center">
+        <a href="https://pipergpt.qlay.xyz" className="underline text-blue-500">JohnPiperGPT</a>
       </div>
     </Container>
   </>
