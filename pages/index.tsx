593a8ed46007baa4834fccc255db58b27ac07028
David Kim <clavid87@gmail.com>
2025-01-27 10:08:40 -0800

    update to studios

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
 // Import the necessary modules
 import { FC } from 'react';
 import Head from 'next/head';
-import Form from '../components/Form';
 import Container from '../components/Container';
 
 // Define the component
 const Home: FC = () => (
   <>
     <Head>
-      <title>Qlay</title>
+      <title>Qlay Studios</title>
       <meta name="description" content="Tools for the sojourner life" />
     </Head>
     <Container>
@@ -16,10 +15,9 @@ const Home: FC = () => (
         <h1 className="text-5xl font-light font-serif mb-4 items-center text-white">QLAY</h1>
       </div>
       <div className="my-8 text-center">
-        <Form />
       </div>
       <div className="my-8 text-center">
-        <a href="https://pipergpt.qlay.xyz" className="text-blue-500">JohnPiperGPT</a>
+        contact: founders at qlaystudios dot xyz
       </div>
     </Container>
   </>
